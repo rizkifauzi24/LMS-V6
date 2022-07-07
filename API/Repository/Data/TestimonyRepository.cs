958c1f7da14498ae5379888df87f8879a22c695d
UmbuReinhard <reinhardumbu66@gmail.com>
2022-07-07 19:18:48 +0700

    BaseMainv6

---
+++ API/Repository/Data/TestimonyRepository.cs
@@ -0,0 +1,19 @@
+﻿using API.Contex;
+using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Repository.Data
+{
+    public class TestimonyRepository : GeneralRepository<MyContex, Testimony, int>
+    {
+        private readonly MyContex _context;
+        public TestimonyRepository(MyContex myContext) : base(myContext)
+        {
+
+        }
+
+    }
+}
